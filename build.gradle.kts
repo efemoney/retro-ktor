3257aaf51e7df5c58bec4370361eb1ba0fab73bc
Efeturi Money <efeturi.money@gmail.com>
2022-04-03 18:51:42 +0300

    Bump deps & gradle & configure compilations options vs tasks options

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
-@file:Suppress("DSL_SCOPE_VIOLATION", "NOTHING_TO_INLINE", "UNCHECKED_CAST")
+@file:Suppress("DSL_SCOPE_VIOLATION", "NOTHING_TO_INLINE", "UNCHECKED_CAST", "MemberVisibilityCanBePrivate")
 
-import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
-import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
-import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.dsl.*
+import org.jetbrains.kotlin.gradle.plugin.KotlinCompilation
+import org.jetbrains.kotlin.gradle.plugin.KotlinTarget
 
 plugins {
   alias(libs.plugins.ksp) apply false
@@ -39,16 +38,23 @@ subprojects {
       }
     }
   }
-
-  tasks.withType<KotlinCompile>().configureEach {
-    kotlinOptions {
-      freeCompilerArgs = freeCompilerArgs + listOf(
-        "-opt-in=kotlin.RequiresOptIn",
-        "-opt-in=kotlin.contracts.ExperimentalContracts",
-        "-opt-in=com.squareup.kotlinpoet.ksp.KotlinPoetKspPreview",
-        "-Xcontext-receivers",
-      )
+  pluginManager.withAnyKotlinPlugin {
+    kotlin.configureEachCompilation {
+      kotlinOptions {
+        freeCompilerArgs = freeCompilerArgs + listOf(
+          "-opt-in=kotlin.RequiresOptIn",
+          "-opt-in=kotlin.contracts.ExperimentalContracts",
+          "-opt-in=com.squareup.kotlinpoet.ksp.KotlinPoetKspPreview",
+          "-Xcontext-receivers",
+        )
+      }
     }
+  }
+
+  dependencies {
+    configurations
+      .matchingImplementation()
+      .configureEach { name(platform(libs.kotlin.bom)) }
   }
 }
 
@@ -75,6 +81,8 @@ fun PluginManager.withKotlinJvm(action: Action<AppliedKotlinPlugin<KotlinJvmProj
 fun PluginManager.withKotlinMultiplatform(action: Action<AppliedKotlinPlugin<KotlinMultiplatformExtension>>) =
   withPlugin("org.jetbrains.kotlin.multiplatform") { action.execute(AppliedKotlinPlugin(this)) }
 
+fun ConfigurationContainer.matchingImplementation() = matching { it.name.contains("implementation", ignoreCase = true) }
+
 fun simpleName(name: String, suffix: String) = if (name == "main") suffix else "$name-$suffix"
 
 class AppliedKotlinPlugin<T : KotlinProjectExtension>(appliedPlugin: AppliedPlugin) : AppliedPlugin by appliedPlugin {
@@ -82,6 +90,25 @@ class AppliedKotlinPlugin<T : KotlinProjectExtension>(appliedPlugin: AppliedPlug
   inline val Project.kotlin get() = extensions.getByName<KotlinProjectExtension>("kotlin") as T
 
   inline fun Project.kotlin(action: Action<T>) = extensions.configure("kotlin", action)
+
+  fun T.configureEachTarget(
+    includeMetadata: Boolean = true,
+    action: Action<KotlinTarget>,
+  ) = when (this) {
+    is KotlinSingleTargetExtension -> action.execute(target)
+    is KotlinMultiplatformExtension -> {
+      if (includeMetadata) action.execute(metadata())
+      targets.configureEach(action)
+    }
+    else -> error("")
+  }
+
+  fun T.configureEachCompilation(
+    includeMetadata: Boolean = true,
+    action: Action<KotlinCompilation<KotlinCommonOptions>>
+  ) = configureEachTarget(includeMetadata) {
+    compilations.configureEach(action)
+  }
 }
 
 // endregion
